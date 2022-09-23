e3f7fad366c124a7fa8844aac1345be461c02531
Pascal Heitz <oodelally.npm@gmail.com>
2022-09-23 09:40:49 +0200

    TLS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
+
+const gethttpsOptionsFromEnv = (): HttpsOptions | undefined => {
+  if (!process.env.USE_TLS) {
+    console.log('Not using TLS (HTTP / WS)');
+    return undefined;
+  }
+  console.log('Using TLS (HTTPS / WSS)');
+  const privateKey = fs.readFileSync('./localhost-key.pem', 'utf8');
+  const certificate = fs.readFileSync('./localhost.pem', 'utf8');
+  return { key: privateKey, cert: certificate };
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: gethttpsOptionsFromEnv(),
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
